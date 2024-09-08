be9049a0720e6f382b47efde175352fc5ce86c9e
Alan <alanghribi@gmail.com>
2024-09-08 10:42:23 +0200

    adding modal to the Update button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import '../src/index.css';
-import Employee from './component/Employees';
+import "../src/index.css";
+import Employee from "./component/Employees";
 import {useState} from 'react'
 
 function App() {
-  const showemployees=true
-  const [Role,setRule] = useState();
-  return (
+  const showemployees = true;
+  
+  const [employees,setEmployees] = useState([
     
-    <div className="App" >
+      {
+        name: "alan",
+        role: "dev",
+        img: "https://images.pexels.com/photos/2505026/pexels-photo-2505026.jpeg?",
+      },
+      {
+        name: "carl",
+        role: "dev",
+        img: "https://images.pexels.com/photos/2505026/pexels-photo-2505026.jpeg?",
+      },
+      {
+        name: "nowa",
+        role: "dev",
+        img: "https://images.pexels.com/photos/2505026/pexels-photo-2505026.jpeg?",
+      },
+    ]);
+  return (
+    <div className="App">
       {showemployees ? (
         <>
-          <input type='text' onChange={(e)=>{
-
-            setRule(e.target.value);
-
-
-          }}></input>
-
-          <Employee name='abby' role='designer'/>
-          <Employee name='alan' role='dev'/>
-          <Employee name='jake' role={Role}/>
-          <Employee name='lana' role=''/>
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
         </>
-        ) : (<p> not found </p>)}
+      ) : (
+        <p> not found </p>
+      )}
     </div>
   );
 }
--- src/component/Employees.js
+++ src/component/Employees.js
@@ -1,9 +1,22 @@
+import EditEmployees from "../component/EditEmployees";
+
+
 function Employee(props) {
   return (
-    <>
-      <h3>Employee {props.name}</h3>
-      <p>{props.role ? props.role : "No role"}</p>
-    </>
+    <div className="py-8 px-8 max-w-sm m-2 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="h-20 w-20 object-cover rounded-full"
+        src={props.img}
+        alt="Woman's Face"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.role}</p>
+        </div>
+        <EditEmployees/>
+      </div>
+    </div>
   );
 }
 export default Employee;
